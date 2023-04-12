1c7afc0606ef6ea70490a943e46ebb0f165605b1
King Macking <matiasatzori@gmail.com>
2023-04-12 08:42:53 -0300

    FIX: Modal background color to be more darker

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ const Modal = ({show, onClose, title, children}) => {
         <>
             {
                 show &&
-                <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-black bg-opacity-80" onClick={onClose}>
+                <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-[#000000] bg-opacity-80" onClick={onClose}>
                     <div className="w-[300px] md:w-[600px] bg-white dark:bg-black rounded-lg shadow-lg max-h-[600px] lg:max-h-fit md:py-4 md:px-8 px-3 py-2" onClick={e => e.stopPropagation()}>
                         <div className="pb-2 md:pb-4">
                             <h3 className="text-2xl font-semibold text-center md:text-4xl dark:text-white font-title">{title.toUpperCase()}</h3>
