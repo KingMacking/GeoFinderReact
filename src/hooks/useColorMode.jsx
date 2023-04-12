91b8bbaf5b03f7d6451f1a9029fa673da9b1ed78
King Macking <matiasatzori@gmail.com>
2023-04-12 09:42:24 -0300

    FEAT: Colormode gets default configuration the first time user opens the web

--- src/hooks/useColorMode.jsx
+++ src/hooks/useColorMode.jsx
@@ -2,11 +2,17 @@ import { useEffect } from "react"
 import useLocalStorage from "./useLocalStorage"
 
 const useColorMode = () => {
-    const [colorMode, setColorMode] = useLocalStorage('color-mode', 'light')
-
+    const [colorMode, setColorMode] = useLocalStorage('color-mode', 'default')
+    
     useEffect(() => {
         const className = 'dark'
         const bodyClasses = window.document.body.classList
+        if((colorMode === 'default') && window.matchMedia('(prefers-color-scheme: dark)').matches){
+            setColorMode('dark')
+        }
+        if((colorMode === 'default') && !window.matchMedia('(prefers-color-scheme: dark)').matches){
+            setColorMode('light')
+        }
         colorMode === 'dark' ? bodyClasses.add(className) : bodyClasses.remove(className)
     }, [colorMode])
 
